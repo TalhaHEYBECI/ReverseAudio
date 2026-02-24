1660e4bff1261a6294224971dcf8d8b412771692
talha heybeci <aheybeci@hotmail.com>
2026-02-24 03:08:08 +0300

    first commit

---
+++ ReverseAudio/ReverseAudio/ReverseAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseAudioApp.swift
+//  ReverseAudio
+//
+//  Created by talha heybeci on 24.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReverseAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
